4be20f5d9e6eb44df86873cc8b0dc05a3e295188
Chris Aquino <chris.aquino@gmail.com>
2017-09-13 13:49:22 -0400

    adds actions, store, and reducer

---
+++ src/actions.js
@@ -0,0 +1,21 @@
+// Tip #1: Make constants for your action type strings.
+
+/*
+
+This is what we want to produce as an action object:
+{
+  type: ADD_MEMBER,
+  name: 'crusher',
+  rank: 'chief medical officer',
+  id: 12345
+}
+
+*/
+
+const ADD_MEMBER = 'ADD_MEMBER';
+const BEAM_MEMBER = 'BEAM_MEMBER';
+
+export default {
+  ADD_MEMBER,
+  BEAM_MEMBER,
+}
