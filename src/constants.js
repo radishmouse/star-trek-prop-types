8f892c2f0632ac6fda4ed61dffc3aef67122f09b
Chris Aquino <chris.aquino@gmail.com>
2017-09-14 14:23:00 -0400

    adds constants

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export const ADD_MEMBER = 'ADD_MEMBER';
+export const BEAM_MEMBER = 'BEAM_MEMBER';
+export const ADD_LOCATION = 'ADD_LOCATION';
+export const ADD_RESOURCE = 'ADD_RESOURCE';
+export const REMOVE_RESOURCE = 'REMOVE_RESOURCE';
+
+export const LOCATION_PLANET = 'planet';
+export const LOCATION_SHIP = 'ship';
