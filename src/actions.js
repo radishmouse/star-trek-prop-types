26df5ba939844f9cdbe11821cc2f3d6206b92028
Chris Aquino <chris.aquino@gmail.com>
2017-09-14 10:59:43 -0400

    notes about refactor in src/actions.js

--- src/actions.js
+++ src/actions.js
@@ -1,3 +1,51 @@
+let thingy = {
+  locations: {
+    planets: {
+      '52': {
+        id: 52,
+        name: 'Pluto',
+        resources: {
+          crystals: 1000
+        }
+      },
+      '54': {
+        id: 54,
+        name: 'Earth',
+        resources: {
+          crystals: 1000
+        }
+      },
+    },
+    ships: {
+      '1000': {
+        id: 1000,
+        name: 'Enterprise',
+        cargo: 500,
+        resources: {
+        }
+      }
+    }
+  },
+  crewMembers: {
+    '300': {
+      id: 300,
+      name: 'Leo',
+      rank: 'That big dude',
+      location: 52
+    }
+  }
+}
+
+/*
+
+
+
+
+
+*/
+
+
+
 // Tip #1: Make constants for your action type strings.
 
 /*
@@ -8,32 +56,46 @@ This is what we want to produce as an action object:
   name: 'crusher',
   rank: 'chief medical officer',
   id: 12345,
-  location: 'PLANET_THE_SUN',
+  location: 54,
 }
 
 {
   type: BEAM_MEMBER,
   id: 12345,
-  location: 'LOCATION_SHIP',
+  location: 1000,
 }
 
 {
   type: ADD_SHIP,
   name: 'Enterprise',
-  id: 54321
+  id: 54321,
+  resources: {
+    cargo: 10000000000
+  }
 }
-
+const LOCATION_PLANET = 'planets'
 {
-  type: ADD_PLANET,
+  type: ADD_LOCATION,
+  locationType: LOCATION_PLANET
   name: 'Earth',
   id: 5432112345
 }
 
 {
-  type: REMOVE_CRYSTALS,
+  type: REMOVE_RESOURCE,
+  resource: 'crystals',
   howMany: 1,
   id: 1
 }
+
+{
+  type: ADD_RESOURCE,
+  resource: 'crystals',
+  howMany: 1,
+  id: 1
+}
+
+
 
 */
 
